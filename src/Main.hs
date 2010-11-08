c6d7db13c20b5a9aab87b408746eb05fa1db393e
Daniel Corson <danl@alum.mit.edu>
2010-11-07 23:26:16 -1000

    fr2.0

--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,21 @@ import Control.Applicative
 import Control.Monad
 import Data.Char
 import Data.List
+import Data.Maybe
 import FUtil
-import HSH
 import System.Console.GetOpt
 import System.Directory
 import System.Environment
 import System.FilePath
 import System.IO
+import System.Process
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Match
 import qualified Data.Map as M
 
 import qualified Opt
+
+-- todo: clear input buffer more often to prevent accidents
 
 type Params = M.Map String String
 
@@ -29,32 +32,36 @@ data State = State {
 startState :: State
 startState = State 0 0 "" ""
 
-getTags :: Maybe Params -> IO [Tag String]
-getTags postDataMb = let
+myRun :: FilePath -> [String] -> IO String
+myRun c as = do
+  -- todo: error checking?
+  (_ec, out, _err) <- readProcessWithExitCode c as ""
+  return out
+
+getPage :: Maybe Params -> IO String
+getPage postDataMb = myRun "wget" args where
+  args = ["-O", "-", "http://freerice.com/game.php"] ++ postDataArgs
   postDataArgs = case postDataMb of
     Nothing -> []
     Just params -> ["--post-data",
       intercalate "&" . map (\ (k, v) -> k ++ "=" ++ v) $ M.toList params]
-  args = ["-O", "-", "http://freerice.com"] ++ postDataArgs
-  in parseTags <$> run ("hide_errs", "wget":args)
 
 tagsToParams :: [Tag String] -> Params
-tagsToParams = let
-  -- I should have used Maybe but List's were briefer; am I going to hell?
-  toPair (TagOpen _ [_, ("name", n), ("value", v)]) = [(n, v)]
-  toPair _ = []
-  in M.fromList . concatMap toPair . filter (isTagOpenName "input")
+tagsToParams = 
+  M.fromList . map toPair . filter (tagOpenLit "input" (const True))
+  where
+  toPair (TagOpen _ a) = 
+    (fromJust $ lookup "name" a, fromJust $ lookup "value" a)
 
 tagsToCorrectStr :: [Tag String] -> Maybe String
-tagsToCorrectStr tags = let
-  incorrectsToEnd = filter
-    (tagOpenAttrNameLit "div" "id" (== "incorrect") . head) .
-    init $ tails tags
-  in case incorrectsToEnd of
-    [] -> Nothing
-    (_:TagText t:_):_ ->
-      Just . reverse . dropWhile isSpace . reverse $ dropWhile isSpace t
-    _ -> error "unexpected response from freerice.."
+tagsToCorrectStr tags = case incorrectsToEnd of
+  [] -> Nothing
+  TagText t:_ ->
+    Just . reverse . dropWhile isSpace . reverse $ dropWhile isSpace t
+  _ -> error "unexpected response from freerice.."
+  where 
+  incorrectsToEnd = drop 1 $ dropWhile
+    (not . tagOpenAttrNameLit "div" "id" (== "incorrect")) tags
 
 collectAnswer :: Opt.Opts -> String -> IO (Maybe Int)
 collectAnswer opts s = do
@@ -69,9 +76,13 @@ collectAnswer opts s = do
       Nothing -> doingItWrong
       Just i -> if i < 1 || i > 4 then doingItWrong else return $ Just i
 
+tagTextMb :: Tag t -> Maybe t
+tagTextMb (TagText t) = Just t
+tagTextMb _ = Nothing
+
 playGame :: Opt.Opts -> Maybe Params -> State -> IO ()
 playGame opts paramsMb (State roundNum rightNum lastWord lastGuess) = do
-  tags <- getTags paramsMb
+  tags <- parseTags <$> getPage paramsMb
   home <- getEnv "HOME"
   (correctStr, rightNum') <- case paramsMb of
     Nothing -> return ("", rightNum)
@@ -82,10 +93,10 @@ playGame opts paramsMb (State roundNum rightNum lastWord lastGuess) = do
         appendFile (home ++ "/.ricer/wrong") (head (words s) ++ "\n")
         return ("*********WRONG!********* " ++ s, rightNum)
   let
+    word = head . catMaybes . map tagTextMb $
+      dropWhile (/= TagOpen "div" [("id","question-title")]) tags
     params = tagsToParams tags
-    -- lol param name..
-    Just wordStr = M.lookup "INFO3" params
-    word:choices = breaks (== '|') wordStr
+    wdList = breaks (== '|') . fromJust $ M.lookup "list" params
     topStr = show rightNum' ++ " / " ++ show roundNum ++ ": " ++ correctStr
     keepAsking = case Opt.tillNRight opts of
       Nothing -> True
@@ -94,13 +105,14 @@ playGame opts paramsMb (State roundNum rightNum lastWord lastGuess) = do
   putStrLn topStr
   when keepAsking $ do
     ans <- collectAnswer opts . unlines $
-      word:"":zipWith (\ n c -> [n] ++ " " ++ c) (Opt.numbering opts) choices
+      word:"":zipWith (\ n c -> [n] ++ " " ++ c) (Opt.numbering opts) wdList
     case ans of
       Nothing -> return ()
       Just i ->
-        playGame opts (Just params') .
-          State (roundNum + 1) rightNum' word $ choices !! (i - 1)
-        where params' = M.insert "SELECTED" (show i) params
+        playGame opts 
+          (Just . M.insert "op" "next" $ 
+            M.insert "answer" (show $ i - 1) params) $
+          State (roundNum + 1) rightNum' word $ wdList !! (i - 1)
 
 usage :: String
 usage = "usage: ricer [options]\n" ++ Opt.optInfo
